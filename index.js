c727be3be23f68e9ea699de5e6fdc4b45bb4bee2
Denis Wachira <deniswachira77@gmail.com>
2023-02-18 03:02:59 +0300

    done

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const contacts = require('./routes/contact');
+
+//middlewares
+app.use(express.json());
+dotenv.config();
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(console.log("connected to mongodb"))
+    .catch((err) => console.log(err));
+
+
+//define my routes
+app.use("/api", contacts);
+
+app.listen(process.env.PORT || 6000, () => {
+    console.log('Server running on port 6000');
+});
